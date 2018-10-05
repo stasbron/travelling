9a5b50d055ac73220d97ca793b5dc32f639ac94c
Stanislav Bronevytskyy <ssbron2@gmail.com>
2018-10-05 06:25:09 +0000

    new commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,57 @@
-$(window).on("scroll", function(){
-    if($(window).scrollTop()){
-        $("nav").addClass("black");
+$(function() {
+
+    var width = $('#container').width();
+    var sliderTimer;
+
+    $('.slaider >li').width(width);
+    $('.slaider').width(width * ('.slaider >li').length);
+
+    //position
+
+    $('.slaider').css('left', -width);
+    $('.slaider>li:last-child').prependTo('.slaider');
+
+    //move
+
+    function nextSlide() {
+        $('.slaider').animate({
+            'margin-left': -width
+        }, 10000, function() {
+            $('.slaider>li:first-child').appendTo('.slaider');
+            $('.slaider').css('margin-left', 0)
+        });
     }
-    else
-        $("nav").removeClass("black");
+
+    //move back
+
+    function prevSlide() {
+        $('.slaider').animate({
+            'margin-left': width
+        }, 10000, function() {
+            $('.slaider>li:last-child').prependTo('.slaider');
+            $('.slaider').css('margin-left', 0)
+        });
+    }
+
+    $('.next').click(nextSlide);
+    $('.prev').click(prevSlide);
+
+    //stop when i want click prv
+
+    $('.prev').click(function() {
+        clearInterval(sliderTimer);
+    });
+
+    $(function() {
+        sliderTimer = setInterval(nextSlide, 5000);
+        $('#container').hover(function() {
+            clearInterval(sliderTimer);
+        }, function() {
+            sliderTimer = setInterval(nextSlide, 5000);
+        });
+    });
+
+
 });
+
+
--- js/smooth.js
+++ js/smooth.js
@@ -1,15 +1,26 @@
 
-$ (function() {
-	$('.smooth').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-           if (target.length) {
-             $('html,body').stop().animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-    }
-});
-});
+    $(function() {
+        $('.smooth').on('click', function(event) {
+            var target = $(this.getAttribute('href'));
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+            }
+        });
+    });
 
-$(function (){
-	new WOW().init();
-});
+
+
+    $(function() {
+        var btn_menu = $('.btn_menu');
+        menu = $('nav ul');
+
+        $(btn_menu).on('click', function(e) {
+            e.preventDefault();
+            menu.slideToggle(1500);
+        });
+    });
+
+
